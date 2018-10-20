06ffaa39f3d20b382d9b4e298459291bbd421326
TJASA <tjasaa@gmail.com>
2018-10-20 14:38:43 +0200

    fix migrations problem

--- IssueTracker/settings.py
+++ IssueTracker/settings.py
@@ -97,6 +97,12 @@ else:
             'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
         }
     }
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 
 
 # Password validation
