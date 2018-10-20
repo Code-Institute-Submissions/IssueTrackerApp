3f7120e4acfec83726329eccf2b723d36dadecc9
TJASA <tjasaa@gmail.com>
2018-10-20 13:27:38 +0200

    add procfile

--- IssueTracker/settings.py
+++ IssueTracker/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'xt5*)#key7o)v0v^0y&qgyl$hrz!qg3oy@$2_y8pr_+g+9)a-l'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
