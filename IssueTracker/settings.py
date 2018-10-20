453b1f79d9e7e7ed5a544f6b8a07cd57f8b45bd1
TJASA <tjasaa@gmail.com>
2018-10-20 22:05:04 +0200

    add email connection

--- IssueTracker/settings.py
+++ IssueTracker/settings.py
@@ -129,6 +129,14 @@ AUTHENTICATION_BACKENDS = [
     'userAuth.backends.EmailAuth'
 ]
 
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = os.environ.get('EMAIL_ADDRESS')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASSWORD')
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+SERVER_EMAIL = EMAIL_HOST_USER
+
 # Internationalization
 # https://docs.djangoproject.com/en/2.1/topics/i18n/
 
