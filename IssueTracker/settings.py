76e4ff857a7290607790b2439d6f36e6891c00f3
TJASA <tjasaa@gmail.com>
2018-10-23 19:23:26 +0200

    fix problem with media files loading

--- IssueTracker/settings.py
+++ IssueTracker/settings.py
@@ -171,13 +171,12 @@ AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
 STATIC_URL = '/static/'
 STATICFILES_LOCATION = 'static'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'), )
-#STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 STATICFILES_STORAGE = 'custom_storages.StaticStorage'
 
 MEDIAFILES_LOCATION = 'media'
 DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
 
-MEDIA_URL = '/media/'
+MEDIA_URL = "https://{}/{}/".format(AWS_S3_CUSTOM_DOMAIN, 'media')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 STRIPE_PUBLISHABLE = os.getenv('STRIPE_PUBLISHABLE')
